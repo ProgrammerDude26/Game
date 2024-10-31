cb663932dc778100b958b0967bdcc42d16a61ff8
Davis Woodring <dwoodr10@wgu.edu>
2024-10-31 11:58:49 -0400

    Automated Tile Laying Process

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,10 +14,10 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; // 16x16 tile 
     final int scale = 3; // scales size for bigger resolution
     public final int tileSize = originalTileSize * scale; // 48x48 tile because 16 * 3
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels width
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels height
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels width
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels height
     // FPS
     int FPS = 60;
 
