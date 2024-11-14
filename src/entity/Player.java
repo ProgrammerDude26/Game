529ad31f37ecf941d111015231a1508711da0897
Davis Woodring <dwoodr10@wgu.edu>
2024-11-14 18:47:48 -0500

    World Map & Draw Distance Optimization only drawing 16 of the correct tile before default

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,17 +15,23 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth / 2 - (gp.tileSize/2);
+        screenY = gp.screenHeight / 2 - (gp.tileSize/2);
 
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues() {
 
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -53,19 +59,19 @@ public class Player extends Entity{
         // UPDATE LOOP
         if(keyH.upPressed == true) {
             direction = "up";
-            y = y - speed;
+            worldY = worldY - speed;
         }
         else if(keyH.downPressed == true) {
             direction = "down";
-            y = y + speed;
+            worldY = worldY + speed;
         }
         else if(keyH.leftPressed == true) {
             direction = "left";
-            x = x - speed;
+            worldX = worldX - speed;
         }
         else if(keyH.rightPressed == true) {
             direction = "right";
-            x = x + speed;
+            worldX = worldX + speed;
         }
 
         spriteCounter++;
@@ -120,7 +126,7 @@ public class Player extends Entity{
             break;
         }
 
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,17 +18,24 @@ public class GamePanel extends JPanel implements Runnable{
     public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels width
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels height
+
+    //WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
     // FPS
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this,keyH);
+    public Player player = new Player(this,keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.darkGray);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true); // Can improve rendering performance
         this.addKeyListener(keyH);
         this.setFocusable(true);
