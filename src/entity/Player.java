e5bd1640710c7d3416ac8e9c19fa90a7178dfc6c
Davis Woodring <dwoodr10@wgu.edu>
2024-11-15 00:58:10 -0500

    World Map Edits, Wont Show Right Half of Map

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -30,8 +30,8 @@ public class Player extends Entity{
     }
     public void setDefaultValues() {
 
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 50;
+        worldY = gp.tileSize * 20;
         speed = 4;
         direction = "down";
     }
