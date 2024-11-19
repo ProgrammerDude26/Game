7a80f15ae95fa50e0ff13a51c548add8335299ba
Davis Woodring <dwoodr10@wgu.edu>
2024-11-18 20:02:44 -0500

    More Item Placement

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -13,20 +13,20 @@ public class AssetSetter {
     public void setObject() {
 
         gp.obj[0] = new OBJ_Key();
-        gp.obj[0].worldX = 2 * gp.tileSize;
-        gp.obj[0].worldY = 7 * gp.tileSize;
+        gp.obj[0].worldX = 21 * gp.tileSize;
+        gp.obj[0].worldY = 11 * gp.tileSize;
 
         gp.obj[1] = new OBJ_Key();
-        gp.obj[1].worldX = 5 * gp.tileSize; 
-        gp.obj[1].worldY = 5 * gp.tileSize;
+        gp.obj[1].worldX = 20 * gp.tileSize; 
+        gp.obj[1].worldY = 10 * gp.tileSize;
 
         gp.obj[2] = new OBJ_Key();
-        gp.obj[2].worldX = 3 * gp.tileSize; 
-        gp.obj[2].worldY = 4 * gp.tileSize;
+        gp.obj[2].worldX = 24 * gp.tileSize; 
+        gp.obj[2].worldY = 5 * gp.tileSize;
 
         gp.obj[3] = new OBJ_Door();
-        gp.obj[3].worldX = 3 * gp.tileSize; 
-        gp.obj[3].worldY = 4 * gp.tileSize;
+        gp.obj[3].worldX = 6 * gp.tileSize; 
+        gp.obj[3].worldY = 7 * gp.tileSize;
 
         gp.obj[4] = new OBJ_Door();
         gp.obj[4].worldX = 9 * gp.tileSize; 
