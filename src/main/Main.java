43fa59e91b73ace0c3dd101689f5dbda810935fd
Davis Woodring <dwoodr10@wgu.edu>
2025-01-16 16:38:49 -0500

    Random Check

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false); // non-resizeable
-        window.setTitle("My First Game"); // Title Screen 
+        window.setTitle("RPG Game - Java"); // Title Screen 
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
